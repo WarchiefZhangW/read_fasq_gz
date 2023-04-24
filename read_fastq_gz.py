2916ead83e3be63d83a5935cd1d7892ed7e0a292
Zhang W <104944688+WarchiefZhangW@users.noreply.github.com>
2023-04-24 19:18:54 +0800

    Update read_fastq_gz.py

--- read_fastq_gz.py
+++ read_fastq_gz.py
@@ -4,26 +4,28 @@ import gzip
 import os,sys
 
 def read_fastq_gz(fp):#this code read *.fastq.gz file
-	name, seq, qual = '', '', ''
-	for line in fp: 
-		l = line.decode()
-		if l[0] in '>@': 
-			if seq != '':
-				yield name, (seq, qual)
-			name = l[: -1]#remove \n
-			seq, qual = '', ''
-			seq_bool, qual_bool = True, False
-			continue
-		if l[0] == '+' and not qual_bool:
-			seq_bool, qual_bool = False, True
-			continue
-		if seq_bool and not qual_bool:
-			seq += l.strip()
-			continue
-		if not seq_bool and qual_bool:
-			qual += l.strip()
-			continue
-	yield name, (seq, qual)#yield the last read
+        name, seq, qual = '', '', ''
+        seq_bool, qual_bool =  False,False
+        for line in fp: 
+                l = line.decode()
+                #print(l[:3], seq_bool, qual_bool)
+                if l[0] == '@' and len(seq) <= len(qual): #fastq qual long enough
+                        if seq != '' and len(seq) == len(qual):#good quality to produce
+                                yield name, (seq, qual)
+                        name = l[: -1]#remove \n, then start a new one
+                        seq, qual = '', ''
+                        seq_bool, qual_bool = True, False
+                        continue
+                if l[0] == '+' and not qual_bool:
+                        seq_bool, qual_bool = False, True
+                        continue
+                if seq_bool and not qual_bool:
+                        seq += l.strip()
+                        continue
+                if not seq_bool and qual_bool:
+                        qual += l.strip()
+                        continue
+        yield name, (seq, qual)#yield the last read
 
 
 
